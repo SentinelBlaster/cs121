ea559af5c34b45a50474dcc64d240c52cffc593c
SentinelBlaster <jpfait2014@gmail.com>
2023-12-07 12:20:28 -0500

    rest of the files

--- src/weekThirteen/linkedListActivity/Main.java
+++ src/weekThirteen/linkedListActivity/Main.java
@@ -1,4 +1,25 @@
 package weekThirteen.linkedListActivity;
 
 public class Main {
+    public static void main(String[] args) {
+        UserBooking john = new UserBooking("SentinelBlaster", "Transformers: Rise of the Beasts", 127, "Action","The sequal to Bumblebee", "Regal");
+        UserBooking alan = new UserBooking("Creepalan", "Five Nights at Freddie's", 110, "Horror","Based on the hit game", "IMAX");
+
+        john.addTheater("Noblesville");
+        john.addTheater("Carmel");
+        john.addTheater("Westfield");
+        john.getUserInfo();
+        john.displayUserBooking();
+        john.removeTheater("Carmel");
+        john.displayUserBooking();
+
+        alan.addTheater("Noblesville");
+        alan.addTheater("Anderson");
+        alan.addTheater("Westfield");
+        alan.getUserInfo();
+        alan.displayUserBooking();
+        alan.removeTheater("Westfield");
+        alan.displayUserBooking();
+
+    }
 }
--- src/weekThirteen/linkedListActivity/UserBooking.java
+++ src/weekThirteen/linkedListActivity/UserBooking.java
@@ -1,4 +1,43 @@
 package weekThirteen.linkedListActivity;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+
 public class UserBooking {
+    private String userName;
+    private String movieTitle;
+    private int runTime;
+    private String genre;
+    private String description;
+    private String theater;
+
+    //Set<String> theaters = new HashSet<>();
+     private LinkedList<String> theaters = new LinkedList<>();
+
+    public UserBooking(String userName, String movieTitle, int runTime, String genre, String description, String theater) {
+        this.userName = userName;
+        this.movieTitle = movieTitle;
+        this.runTime = runTime;
+        this.genre = genre;
+        this.description = description;
+        this.theater = theater;
+    }
+
+    public void addTheater(String newTheater){
+        this.theaters.add(newTheater);
+    }
+
+    public void removeTheater(String oldTheater){
+        this.theaters.remove(oldTheater);
+    }
+
+    public void getUserInfo(){
+        System.out.println(userName);
+        //I wasn't sure what counted for "user info"
+    }
+
+    public void displayUserBooking(){
+        System.out.println(theaters);
+    }
 }
